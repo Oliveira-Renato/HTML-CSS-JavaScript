7b8681f719958767b3a8a8a2913d4a48810512c2
Renato Oliveira <renato.printf@gmail.com>
2021-11-06 20:26:24 -0300

    can be usefull in the future

---
+++ Praticando/Praticando_JS/interesting/script.js
@@ -0,0 +1,26 @@
+//Nova variável "cep" somente com dígitos.
+var cep = valor.replace(/\D/g, '');
+
+//Verifica se campo cep possui valor informado.
+if (cep !== "") {
+
+  //Expressão regular para validar CEP
+  if(validacep.test(cep)) {
+
+    //preenche os campos com "..." enquanto consulta webservice.
+    document.getElementById('endereço').value="...";
+    document.getElementById('bairro').value="...";
+    document.getElementById('cidade').value="...";
+    document.getElementById('estado').value="...";
+
+    //Cria um elemento javascript.
+    var script = document.createElement('script');
+
+    //Sincroniza com o callback
+    script.src = '//viacep.com.br/ws/'+ cep +'/json/?callback=meu_callback';
+
+    //Insere script no documento  e carrega o conteúdo
+    document.body.appendChild(script);
+  }
+  
+}
